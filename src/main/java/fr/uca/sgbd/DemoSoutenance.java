68e1ef26b1872f74a1c438e0d7f56c3f0ab529bd
tahasimse <tahabsgs@gmail.com>
2026-01-08 17:46:03 +0100

    -tps

--- src/main/java/fr/uca/sgbd/DemoSoutenance.java
+++ src/main/java/fr/uca/sgbd/DemoSoutenance.java
@@ -2,9 +2,6 @@ package fr.uca.sgbd;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.Scanner;
 
 /**
